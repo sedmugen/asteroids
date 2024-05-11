3cee4e429cfeb6e8033d999a70791f6a82d5d7e2
TheIllumi <saadmughal321@gmail.com>
2024-05-11 23:21:02 +0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool _thrusting;
+    private float _turnDirection;
+    
+    private void Update()
+    {
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDirection = 1.0f;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+
+        }
+    }
+}
